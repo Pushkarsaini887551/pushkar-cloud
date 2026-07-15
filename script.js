21eb1cd465c547765c18bf180383a6d792669481
Pushkarsaini887551 <PUSHKARSAINI358@GMAIL.COM>
2026-07-15 22:05:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const loginBtn = document.getElementById("loginBtn");
+    const signupBtn = document.getElementById("signupBtn");
+
+    loginBtn.addEventListener("click", () => {
+        alert("Login page is coming soon...");
+    });
+
+    signupBtn.addEventListener("click", () => {
+        alert("Signup page is coming soon...");
+    });
+
+});
